b1727cad8cc4be4484e147ed626fb2d7f7ee0ccd
S1fTred <matvei.tchufistov@gmail.com>
2023-09-27 15:00:14 +0300

    3-d

--- Prakt_ChufistovMV.py
+++ Prakt_ChufistovMV.py
@@ -38,17 +38,52 @@ player_dx = 0
 player_x = screen_width/2 - player_width/2
 player_y = screen_height  - player_height - player_gap
 
-running = True
-while running:
-    # изменение модели
-    player_x += player_dx
+# пуля
+bullet_img = pg.image.load('src/bullet.png')
+bullet_width, bullet_height = bullet_img.get_size()
+bullet_dy = -5
+bullet_x = player_x     # микро дз - пускать из середины
+bullet_y = player_y - bullet_height
+bullet_alive = False    # есть пуля?
 
-    # redraw
+def model_update():
+    palayer_model()
+    bullet_model()
+
+def palayer_model():
+    x = 7   # создание переменной и ее инициализация
+    x = 7   # изменение значения уже созданной переменнной
+    global player_x
+    player_x += player_dx
+    if player_x < 0:
+        player_x = 0
+    elif player_x > screen_width - player_width:
+        player_x = screen_width - player_width
+
+def bullet_model():
+    global bullet_y, bullet_alive
+    bullet_y += bullet_dy
+    # пуля улетела за верх экрана
+    if bullet_y < 0:
+        bullet_alive = False
+
+def bullet_create():
+    global bullet_y, bullet_x, bullet_alive
+    bullet_alive = True
+    bullet_x = player_x  # микро дз - пускать из середины
+    bullet_y = player_y - bullet_height
+
+
+def display_redraw():
     display.blit(bg_img, (0, 0))
     display.blit(player_img, (player_x, player_y))
+    if bullet_alive:
+        display.blit(bullet_img, (bullet_x, bullet_y))
     pg.display.update()
 
-    # обработка событий
+def event_processing():
+    global player_dx
+    running = True
     for event in pg.event.get():
         # нажали крестик на окне
         if event.type == pg.QUIT:
@@ -58,7 +93,7 @@ while running:
             # нажали на q - quit
             if event.key == pg.K_q:
                 running = False
-        # движ игрока
+        # движение игрока
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_a or event.key == pg.K_LEFT:
                 player_dx = -player_velocity
@@ -68,6 +103,15 @@ while running:
             player_dx = 0
 
 
+
     clock.tick(FPS)
+    return running
+
+
+running = True
+while running:
+    model_update()
+    display_redraw()
+    running = event_processing()
 
 pg.quit()
