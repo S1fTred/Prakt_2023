5eaea4f31619032050ee196b5c03eacc79ea3411
unknown <matvei.tchufistov@gmail.com>
2023-09-27 14:44:30 +0300

    2-nd

--- Prakt_ChufistovMV.py
+++ Prakt_ChufistovMV.py
@@ -1,65 +1,73 @@
 import pygame as pg
 
+
 pg.init()
 
 screen_width, screen_height = 800, 600
 
-FPS = 24
+FPS = 24    # frame per second
 clock = pg.time.Clock()
 
-# Изображения
+# изображения
 bg_img = pg.image.load('src/background.png')
 icon_img = pg.image.load('src/ufo.png')
 
 display = pg.display.set_mode((screen_width, screen_height))
 pg.display.set_icon(icon_img)
-pg.display.set_caption('Starfield')
+pg.display.set_caption('Космическое вторжение')
 
-sys_font = pg.font.SysFont('arial', 35)
-font = pg.font.Font('src/04B_19.TTF', 100)
-
+sys_font = pg.font.SysFont('arial', 34)
+font = pg.font.Font('src/04B_19.TTF', 48)
 
 # display.fill('blue', (0, 0, screen_width, screen_height))
-display.blit(bg_img, (0, 0))
+display.blit(bg_img, (0, 0))        # image.tr
 
-text_img = sys_font.render(f'Score 357', True, 'yellow')
-display.blit(text_img, (350, 50))
+text_img = sys_font.render('Score 123', True, 'white')
+# display.blit(text_img, (100, 50))
 
-game_over_text = font.render('Game over', True, 'yellow')
+game_over_text = font.render('Game Over', True, 'red')
 w, h = game_over_text.get_size()
-display.blit(game_over_text, (screen_width/2 - w/2, screen_height/2 - h/2))
+# display.blit(game_over_text, (screen_width/2 - w/2, screen_height / 2 - h/2))
 
 # игрок
 player_img = pg.image.load('src/player.png')
 player_width, player_height = player_img.get_size()
+player_gap = 10
+player_velocity = 10
+player_dx = 0
+player_x = screen_width/2 - player_width/2
+player_y = screen_height  - player_height - player_gap
 
 running = True
-flag = True
 while running:
+    # изменение модели
+    player_x += player_dx
+
+    # redraw
+    display.blit(bg_img, (0, 0))
+    display.blit(player_img, (player_x, player_y))
     pg.display.update()
+
+    # обработка событий
     for event in pg.event.get():
         # нажали крестик на окне
         if event.type == pg.QUIT:
             running = False
         # тут нажимаем на клавиши
         if event.type == pg.KEYDOWN:
-            # нажали на q - qlose
+            # нажали на q - quit
             if event.key == pg.K_q:
                 running = False
-            if event.key == pg.K_SPACE:
-                display.blit(bg_img, (0, 0))
-            if event.key == pg.K_F5:
-                if flag:
-                    flag = False
-                    snapshot = display.copy()
-                    player_name_text = sys_font.render("Матвей", True, 'yellow')
-                    a = display.blit(player_name_text, (350, 500))
-                else:
-                    flag = True
-                    display.blit(snapshot, a, a)
+        # движение игрока
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_a or event.key == pg.K_LEFT:
+                player_dx = -player_velocity
+            if event.key == pg.K_d or event.key == pg.K_RIGHT:
+                player_dx = player_velocity
+        if event.type == pg.KEYUP:
+            player_dx = 0
+
 
     clock.tick(FPS)
 
 pg.quit()
-
-
